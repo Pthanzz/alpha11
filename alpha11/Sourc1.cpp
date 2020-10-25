928764fdf70ba0bc4a8a5cb722ededbbbf15e034
THANYALAK AMPAI <63010459@KMITL.AC.TH>
2020-10-26 02:33:23 +0700

    Add project files.

---
+++ alpha11/Sourc1.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char rubbe[100], aou[30], maiaou[30];
+	char* pa;
+	scanf_s("%s", &rubbe);
+	for (int n = 0; n < strlen(rubbe); n++) {
+		if (rubbe[n] >= 'a' && rubbe[n] <= 'z') {
+			rubbe[n] -= 32;
+			printf("%c", rubbe[n]);}
+		else if (rubbe[n] >= 'A' && rubbe[n] <= 'Z'){
+			rubbe[n] += 32;
+			printf("%c", rubbe[n]);}
+	}
+	return 0;
+}//นับเป็นนวีค11ได้ไหมคะ นะนะพลีสส*^*
